c819e5edb4963d29906e38b48e614015d5c7d43a
Gabriel <iamjoaoaquino@gmail.com>
2025-08-03 20:31:59 +0200

    DatabaseUserOperations abstraction has been created

--- apps/api/src/routes/user.ts
+++ apps/api/src/routes/user.ts
@@ -1,39 +1,55 @@
 import type { FastifyInstance } from "fastify";
-import { prisma } from "db";
 import type { User } from "db/types";
+import { DatabaseUserOperations } from "db";
 
 export default async function userRoutes(fastify: FastifyInstance) {
   fastify.post("/users", async (req, res) => {
-    const { password, email, role } = req.body as User;
-    const user = await prisma.user.create({ data: { password, email, role } });
-    return res.code(201).send(user);
+    const newUser = req.body as User;
+    try {
+      return res
+        .code(201)
+        .send(await DatabaseUserOperations.createOne(newUser));
+    } catch (error) {
+      return res.code(500).send(error);
+    }
   });
 
   fastify.get("/users", async (_, res) => {
-    const users = await prisma.user.findMany();
-    return res.send(users);
+    try {
+      return res.send(await DatabaseUserOperations.findMany());
+    } catch (error) {
+      return res.code(500).send(error);
+    }
   });
 
   fastify.get("/users/:id", async (req, res) => {
     const { id } = req.params as { id: string };
-    const user = await prisma.user.findUnique({ where: { id } });
-    if (!user) return res.code(404).send({ error: "User not found" });
-    return res.send(user);
+    try {
+      const user = await DatabaseUserOperations.findOne(id);
+      if (!user) return res.code(404).send({ error: "User not found" });
+      return res.send(user);
+    } catch (error) {
+      return res.code(500).send(error);
+    }
   });
 
   fastify.put("/users/:id", async (req, res) => {
     const { id } = req.params as { id: string };
-    const { password, email, role } = req.body as User;
-    const user = await prisma.user.update({
-      where: { id },
-      data: { password, email, role },
-    });
-    return res.send(user);
+    const user = req.body as User;
+    try {
+      return res.send(await DatabaseUserOperations.updateOne(id, user));
+    } catch (error) {
+      return res.code(500).send(error);
+    }
   });
 
   fastify.delete("/users/:id", async (req, res) => {
     const { id } = req.params as { id: string };
-    await prisma.user.delete({ where: { id } });
-    return res.code(204).send();
+    try {
+      await DatabaseUserOperations.deleteOne(id);
+      return res.code(204).send();
+    } catch (error) {
+      return res.code(500).send(error);
+    }
   });
 }
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,3 +1,24 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, type User } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const prisma = new PrismaClient();
+
+export const DatabaseUserOperations = {
+  createOne: async (newUser: User) => {
+    return await prisma.user.create({ data: newUser });
+  },
+  deleteOne: async (id: string) => {
+    return await prisma.user.delete({ where: { id } });
+  },
+  updateOne: async (id: string, user: User) => {
+    return await prisma.user.update({
+      where: { id },
+      data: user,
+    });
+  },
+  findMany: async () => {
+    return await prisma.user.findMany();
+  },
+  findOne: async (id: string) => {
+    return await prisma.user.findUnique({ where: { id } });
+  },
+};
