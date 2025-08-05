20b46cebcaca594f25a6741b0b143b5f778e638c
Gabriel <iamjoaoaquino@gmail.com>
2025-08-05 22:34:24 +0200

    User routes documented with Swagger

--- apps/api/src/routes/user.ts
+++ apps/api/src/routes/user.ts
@@ -1,13 +1,30 @@
 import type { FastifyInstance } from "fastify";
 import type { User } from "db/types";
+import {
+  CreateUserSchema,
+  CreateUserResponseSchema,
+  CreateUserType,
+  UserSchema,
+  UpdateUserSchema,
+} from "db/schemas/User";
 import { DatabaseUserOperations } from "db";
 
 export default async function userRoutes(fastify: FastifyInstance) {
   fastify.post(
     "/users",
-    { preHandler: fastify.authenticate },
+    {
+      preHandler: fastify.authenticate,
+      schema: {
+        tags: ["User"],
+        summary: "Create new user",
+        body: CreateUserSchema,
+        response: {
+          201: CreateUserResponseSchema,
+        },
+      },
+    },
     async (req, res) => {
-      const newUser = req.body as User;
+      const newUser = req.body as CreateUserType;
       try {
         return res
           .code(201)
@@ -18,28 +35,65 @@ export default async function userRoutes(fastify: FastifyInstance) {
     }
   );
 
-  fastify.get("/users", async (_, res) => {
-    try {
-      return res.send(await DatabaseUserOperations.findMany());
-    } catch (error) {
-      return res.code(500).send(error);
+  fastify.get(
+    "/users",
+    {
+      schema: {
+        tags: ["User"],
+        summary: "Get all users",
+        response: {
+          201: {
+            type: "array",
+            items: UserSchema,
+          },
+        },
+      },
+    },
+    async (_, res) => {
+      try {
+        return res.send(await DatabaseUserOperations.findMany());
+      } catch (error) {
+        return res.code(500).send(error);
+      }
     }
-  });
+  );
 
-  fastify.get("/users/:id", async (req, res) => {
-    const { id } = req.params as { id: string };
-    try {
-      const user = await DatabaseUserOperations.findOne(id);
-      if (!user) return res.code(404).send({ error: "User not found" });
-      return res.send(user);
-    } catch (error) {
-      return res.code(500).send(error);
+  fastify.get(
+    "/users/:id",
+    {
+      schema: {
+        tags: ["User"],
+        summary: "Get a specific user by id",
+        response: {
+          201: UserSchema,
+        },
+      },
+    },
+    async (req, res) => {
+      const { id } = req.params as { id: string };
+      try {
+        const user = await DatabaseUserOperations.findOne(id);
+        if (!user) return res.code(404).send({ error: "User not found" });
+        return res.send(user);
+      } catch (error) {
+        return res.code(500).send(error);
+      }
     }
-  });
+  );
 
   fastify.put(
     "/users/:id",
-    { preHandler: fastify.authenticate },
+    {
+      preHandler: fastify.authenticate,
+      schema: {
+        tags: ["User"],
+        summary: "Update specific user by id",
+        body: UpdateUserSchema,
+        response: {
+          201: CreateUserResponseSchema,
+        },
+      },
+    },
     async (req, res) => {
       const { id } = req.params as { id: string };
       const user = req.body as User;
@@ -53,7 +107,20 @@ export default async function userRoutes(fastify: FastifyInstance) {
 
   fastify.delete(
     "/users/:id",
-    { preHandler: fastify.authenticate },
+    {
+      preHandler: fastify.authenticate,
+      schema: {
+        tags: ["User"],
+        summary: "Delete specific user by id",
+        response: {
+          204: {
+            type: "null",
+            description: "User deleted successfully",
+          },
+        },
+      },
+    },
+
     async (req, res) => {
       const { id } = req.params as { id: string };
       try {
