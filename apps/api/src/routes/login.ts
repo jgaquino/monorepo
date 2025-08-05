89e98fa456ca20d6d11e5ff96df0a70e4f80d9a8
Gabriel <iamjoaoaquino@gmail.com>
2025-08-05 22:33:32 +0200

    Login route documented with Swagger

--- apps/api/src/routes/login.ts
+++ apps/api/src/routes/login.ts
@@ -3,20 +3,45 @@ import { sign } from "fastify-auth-jwt";
 import { DatabaseUserOperations } from "db";
 
 export default function loginRoute(fastify: FastifyInstance) {
-  fastify.post("/login", async (req, res) => {
-    try {
-      const { email, password } = req.body as {
-        email: string;
-        password: string;
-      };
-      const user = await DatabaseUserOperations.findOne(null, email);
-      if (user?.password !== password)
-        return res.status(401).send("Unauthorized");
+  fastify.post(
+    "/login",
+    {
+      schema: {
+        tags: ["User authentication"],
+        summary: "User authentication with JWT",
+        body: {
+          type: "object",
+          required: ["email", "password"],
+          properties: {
+            email: { type: "string" },
+            password: { type: "string" },
+          },
+        },
+        response: {
+          201: {
+            type: "object",
+            properties: {
+              token: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    async (req, res) => {
+      try {
+        const { email, password } = req.body as {
+          email: string;
+          password: string;
+        };
+        const user = await DatabaseUserOperations.findOne(null, email);
+        if (user?.password !== password)
+          return res.status(401).send("Unauthorized");
 
-      const token = sign(fastify, user);
-      res.status(200).send({ token });
-    } catch (error) {
-      res.status(500).send(error);
+        const token = sign(fastify, user);
+        res.status(200).send({ token });
+      } catch (error) {
+        res.status(500).send(error);
+      }
     }
-  });
+  );
 }
